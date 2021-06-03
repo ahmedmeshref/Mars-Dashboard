1630b59044e0f0cec6c932b8f657d312b13af598
ahmedmeshref <a.meshref@alustudent.com>
2021-06-03 13:38:08 +0200

    Create an API key

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');
+const Immutable = require('immutable');
+const fetch = require('node-fetch');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
