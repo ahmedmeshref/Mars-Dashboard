699d582000b738a61885b6b320f39385edbebd1c
ahmedmeshref <a.meshref@alustudent.com>
2021-06-04 23:47:36 +0200

    Lower case rover name in api call

--- src/server/index.js
+++ src/server/index.js
@@ -21,7 +21,7 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 app.get('/rover', async (req, res) => {
     try {
         console.log(req.body);
-        const rover_name = req.body.rover_name;
+        const rover_name = req.body.rover_name.toLowerCase();
         const page = req.body.page || 1;
         const rover_info = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover_name}/photos?sol=1000&page=${page}&api_key=${process.env.API_KEY}`)
             .then(res => res.json())
