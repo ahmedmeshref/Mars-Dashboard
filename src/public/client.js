87d2eeb581d4e10fd1eca09b33d0361e66060480
ahmedmeshref <a.meshref@alustudent.com>
2021-06-05 02:55:23 +0200

    Implement get rover images logic

--- src/public/client.js
+++ src/public/client.js
@@ -1,10 +1,10 @@
 const store = {
     currRover: 'Curiosity',
-    roverImages: []
+    roversImages: []
 }
 
-// add our markup to the page
 const root = document.getElementById('root');
+const rovers = document.getElementsByClassName('btn');
 
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState);
@@ -16,49 +16,64 @@ const render = async (root, state) => {
 }
 
 
+// Populate the dom with 
+// Launch Date rover.launch_date
+// Landing Date rover.landing_date
+// Status rover.status
+// Most recently available photos img_src
+// Date the most recent photos were taken earth_date
+
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
-
+    const { roversImages } = state;
+    const htmlRovers = roversImages.map(roverInfo => formatRover(roverInfo)).join('');    
     return `
-    `
+        <ul class='rover_lst'>
+            ${htmlRovers}
+        </ul>
+    `;
 }
+
+const formatRover = (roverObj => {
+    return `
+        <li>
+            <p>Launch Date: ${roverObj.rover.launch_date}</p>  
+            <p>landing Date: ${roverObj.rover.landing_date}</p>  
+            <p>Earth Date: ${roverObj.earth_date}</p>  
+            <p>Status: ${roverObj.rover.status}</p> 
+            <img class='rover_img' src='${roverObj.img_src}' alt='rover_img'> 
+        </li>
+    `
+})
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    // render(root, store);
+    tabClick(Array.from(rovers), store);
     getRoverImages(store);
+    render(root, store);
 })
 
-// ------------------------------------------------------  COMPONENTS
+// listening for a click event on exisitng rover btns 
+const tabClick = async (rovers, state) => {
+    rovers.forEach(rover => {
+        rover.addEventListener('click', async e => {
+            const currRoverName = e.target.id;
+            await updateActiveRover(rovers, currRoverName);
+            await updateStore(state, { currRover: currRoverName });
+            await getRoverImages(store);
+        })
+    });
+}
 
-
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date();
-    const photodate = new Date(apod.date);
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate()) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
+const updateActiveRover = (rovers, currRoverName) => {
+    // const oldActiveRover = document.getElementsByClassName('btn active');
+    rovers.forEach(rover => {
+        if (rover.id == currRoverName) {
+            rover.classList.add('active');
+        } else {
+            rover.classList.remove('active');
+        }
+    })
 }
 
 // ------------------------------------------------------  API CALLS
@@ -77,19 +92,19 @@ const getRoverImages = (state) => {
         })
     })
         .then(res => res.json())
-        // .then(roverInfo => updateStore(store, { roverInfo["rover_info"]["photos"] }))
-        .then(roverInfo => console.log(roverInfo['rover_info']['photos']))
+        .then(roversInfo => {
+            try {
+                const roversImages = roversInfo['rover_info']['photos'];
+                return updateStore(store, { roversImages });
+            } catch (err) {
+                console.log(err);
+            }
 
-    return rover_name;
+        })
 }
 
 
-// Populate the dom with 
-// Launch Date
-// Landing Date
-// Status
-// Most recently available photos
-// Date the most recent photos were taken
+
 
 /*
 {
